1a3082c44ecdee8d13c1cd52d7382f51c4fd6493
Jyoti Tumsare <jyoti.tumsare@click2cloud.net>
2020-09-14 18:05:21 +0530

    Update setEnvVariable.ps1

--- setEnvVariable.ps1
+++ setEnvVariable.ps1
@@ -1,5 +1,4 @@
 Param(
-
 [string]$sauceUserName
 )
 Write-Output "sauce.userName value from ADO was passed as a Argument in the ADO Task called $env:SAUCE_USERNAME " +
@@ -7,7 +6,4 @@ Write-Output "sauce.userName value from ADO was passed as a Argument in the ADO 
 [Environment]::SetEnvironmentVariable("SAUCE_USERNAME", "$sauceUserName", "User")
 [Environment]::SetEnvironmentVariable("SAUCE_USERNAME", "$sauceUserName",[System.EnvironmentVariableTarget]::Machine)
 
-
-Write-Output "sauce.sauceAccessKey value from ADO was passed as a Argument in the ADO Task called $env:SAUCE_USERNAME " +
-"to sauceUserName variable in the Posh. This is the value found=>nvironment.GetEnvironmentVariable("SAUCE_USERNAME");"
 
